8cb5c263e7125500c62341b442a4e79691bab545
Lynkx <92759266+Lynkx119@users.noreply.github.com>
2022-08-11 18:55:32 -0300

    lista perro python

---
+++ perro.py
@@ -0,0 +1,7 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+perro=[13,"Puppy","12/12/2012","Macho",123]
+perro[2]="13/12/2012"
+perro[4]=28957346
+print(perro)
+[13, 'Puppy', '13/12/2012', 'Macho', 28957346]
