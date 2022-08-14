0a78cdfb5e485c4eb2cfd435820c182abe71fbab
frj0 <touskekunn@gmail.com>
2022-08-14 17:51:08 -0300

    perro.py

---
+++ perro.py
@@ -0,0 +1,12 @@
+Id_Perro = 13
+nombre = "Puppy"
+fechanacimiento = "12/12/2012"
+sexo = "macho"
+dnidueño = 123
+
+perro = [Id_Perro, nombre, fechanacimiento, sexo, dnidueño]
+print (perro)
+
+perro[2] ="13/12/2012"
+perro [4] = 28957346
+print (perro)
